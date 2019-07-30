5de08802ede13d7bcf1403df3170a490b455a2e0
Ikaruga <ahjo_bb@hotmail.com>
2019-07-30 10:05:37 +0800

    update

---
+++ Test_Algorithm/HeapSort/HeapSort.cpp
@@ -0,0 +1,63 @@
+﻿// HeapSort.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+void printVectorInt(vector<int> &nums)
+{
+	for (auto i : nums) cout << i << " ";
+	cout << endl;
+}
+
+void Heapify(vector<int> &nums, int i, int heap_size)
+{
+	int l = i * 2 + 1;
+	int r = i * 2 + 2;
+	if (l < heap_size && nums[i] < nums[l])
+	{
+		swap(nums[i], nums[l]);
+		printVectorInt(nums);
+		Heapify(nums, l, heap_size);
+	}
+	if (r < heap_size && nums[i] < nums[r])
+	{
+		swap(nums[i], nums[r]);
+		printVectorInt(nums);
+		Heapify(nums, r, heap_size);
+	}
+}
+
+void HeapSort(vector<int> &nums)
+{
+	int heap_size = nums.size();
+	for (int i = heap_size / 2 - 1; i >= 0; i--)
+	{
+		Heapify(nums, i, heap_size);
+	}
+
+	while (heap_size > 0)
+	{
+		swap(nums[0], nums[heap_size - 1]);
+		printVectorInt(nums);
+		heap_size--;
+		for (int i = heap_size / 2 - 1; i >= 0; i--)
+		{
+			Heapify(nums, i, heap_size);
+		}
+	}
+}
+
+int main()
+{
+	vector<int> nums = { 5, 2, 9, 4, 7, 6, 1, 3, 8 };
+	printVectorInt(nums);
+	HeapSort(nums);
+	cout << "堆排序结果：" << endl;
+	printVectorInt(nums);
+	return 0;
+}
--- Test_Algorithm/Quicksort/Quicksort.cpp
+++ Test_Algorithm/Quicksort/Quicksort.cpp
@@ -5,60 +5,96 @@
 #include <iostream>
 #include <time.h>  
 
-void swap(int &a, int &b)
-{
- 	if (a == b) return;	// 防止a和b同一地址而出现问题
-	a ^= b;
-	b ^= a;
-	a ^= b;
-}
+#include <vector>
 
-int Partition(int A[], int p, int r)
+using namespace std;
+
+//void swap(int &a, int &b)
+//{
+// 	if (a == b) return;	// 防止a和b同一地址而出现问题
+//	a ^= b;
+//	b ^= a;
+//	a ^= b;
+//}
+//
+//int Partition(int A[], int p, int r)
+//{
+//	int k = p;
+//	for (int i = p + 1; i < r; i++)
+//	{
+//		if (A[i] < A[r])
+//		{
+//			swap(A[k], A[i]);
+//			k += 1;
+//		}
+//	}
+//	swap(A[k], A[r]);
+//	return k;
+//}
+//
+//void QuickSort(int A[], int p, int r)
+//{
+//	if (p < r)
+//	{
+//		int q = Partition(A, p, r);
+//		QuickSort(A, p, q - 1);
+//		QuickSort(A, q + 1, r);
+//	}
+//}
+
+int Partition(vector<int> &nums, int l, int r)
 {
-	int k = p;
-	for (int i = p + 1; i < r; i++)
+	int m = l;
+	for (int i = l; i < r; i++)
 	{
-		if (A[i] < A[r])
-		{
-			swap(A[k], A[i]);
-			k += 1;
-		}
+		if (nums[i] < nums[r]) swap(nums[m++], nums[i]);
 	}
-	swap(A[k], A[r]);
-	return k;
+	swap(nums[m], nums[r]);
+	return m;
 }
 
-void QuickSort(int A[], int p, int r)
+void QuickSort(vector<int> &nums, int l, int r)
 {
-	if (p < r)
+	if (l < r)
 	{
-		int q = Partition(A, p, r);
-		QuickSort(A, p, q - 1);
-		QuickSort(A, q + 1, r);
+		int m = Partition(nums, l, r);
+		QuickSort(nums, l, m - 1);
+		QuickSort(nums, m + 1, r);
 	}
 }
 
 int main()
 {
 	srand((unsigned)time(NULL));
-
 	const int cNumMax = 10;
-	int iTest[cNumMax];
 
-	std::cout << "iTest = ";
-	for (int i = 0; i < cNumMax; i++)
-	{
-		iTest[i] = rand() % cNumMax;
-		std::cout << iTest[i] << " ";
-	}
-	std::cout << std::endl;
+	//int iTest[cNumMax];
+	//cout << "iTest = ";
+	//for (int i = 0; i < cNumMax; i++)
+	//{
+	//	iTest[i] = rand() % cNumMax;
+	//	cout << iTest[i] << " ";
+	//}
+	//cout << endl;
 
-	QuickSort(iTest, 0, cNumMax - 1);
-	std::cout << "QuickSort = ";
-	for (int i = 0; i < cNumMax; i++)
-	{
-		std::cout << iTest[i] << " ";
-	}
-	std::cout << std::endl;
+	//QuickSort(iTest, 0, cNumMax - 1);
+	//cout << "QuickSort = ";
+	//for (int i = 0; i < cNumMax; i++)
+	//{
+	//	cout << iTest[i] << " ";
+	//}
+	//cout << endl;
+
+
+	//vector<int> nums{ 2,4,0,3,7,8,6,9,1,5 };
+	vector<int> nums{ 2,3,0,3,3,8,6,9,1,3 };
+	cout << "nums = ";
+	for (auto i : nums) cout << i << " ";
+	cout << endl;
+
+	QuickSort(nums, 0, nums.size() - 1);
+	cout << "QuickSort = ";
+	for (auto i : nums) cout << i << " "; 
+	cout << endl;
 }
 

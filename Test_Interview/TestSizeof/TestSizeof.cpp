6f75bb1962cb03328d6578233151975b22608402
Ikaruga <ahjo_bb@hotmail.com>
2019-08-23 15:18:34 +0800

    update

--- Test_Interview/TestSizeof/TestSizeof.cpp
+++ Test_Interview/TestSizeof/TestSizeof.cpp
@@ -4,6 +4,7 @@
 #include "pch.h"
 #include <iostream>
 
+using namespace std;
 
 class A
 {
@@ -15,12 +16,72 @@ public:
 	short d;
 };
 
+struct Empty
+{
+
+};
+
+struct Test
+{
+	int a;
+	char b;
+	double c[10];
+	char d;
+};
+
+struct Test_bit
+{
+	int a : 8;
+	int b : 8;
+	int c : 8;
+	char f;
+	int d : 8;
+	int e : 8;
+	char g;
+};
+
+// 字节对齐
 int main()
 {
-	std::cout << "A" << sizeof(A) << std::endl;
+	cout << endl;
+	cout << "sizeof(Empty) = " << sizeof(Empty) << endl;
 
-	//std::cout << "char" << sizeof(char) << std::endl;
-	//std::cout << "int" << sizeof(int) << std::endl;
-	//std::cout << "double *" << sizeof(double *) << std::endl;
-	//std::cout << "short" << sizeof(short) << std::endl;
+	cout << endl;
+	cout << "sizeof(Test) = " << sizeof(Test) << endl;
+
+	cout << endl;
+	cout << "sizeof(int) = " << sizeof(int) << endl;
+
+	cout << endl;
+	cout << "sizeof(double) = " << sizeof(double) << endl;
+
+	Test Obj;
+	cout << endl << "Test Obj;" << endl;
+	cout << "sizeof(Obj) = " << sizeof(Obj) << endl;
+
+	Test *pObj;
+	cout << endl << "Test *pObj;" << endl;
+	cout << "sizeof(pObj) = " << sizeof(pObj) << endl;
+
+	std::shared_ptr<Test> ptr = std::make_shared<Test>();
+	cout << endl << "std::shared_ptr<Test> ptr = std::make_shared<Test>();" << endl;
+	cout << "sizeof(ptr) = " << sizeof(ptr) << endl;
 }
+
+//////////////////////////////////////////////////////////////////////////
+// 位域字节对齐
+//int main()
+//{
+//	cout << "sizeof(Test_bit) = " << sizeof(Test_bit) << endl;
+//}
+
+//////////////////////////////////////////////////////////////////////////
+//int main()
+//{
+//	std::cout << "A " << sizeof(A) << std::endl;
+//
+//	//std::cout << "char" << sizeof(char) << std::endl;
+//	//std::cout << "int" << sizeof(int) << std::endl;
+//	//std::cout << "double *" << sizeof(double *) << std::endl;
+//	//std::cout << "short" << sizeof(short) << std::endl;
+//}
---
+++ Test_Interview/test_memcpy/test_memcpy.cpp
@@ -0,0 +1,50 @@
+﻿// test_memcpy.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+void * memcpy(void *pDst, void *pSrc, int nLength)
+{
+	if (pDst == nullptr || pSrc == nullptr) return pDst;
+	if (pDst == pSrc) return pDst;
+
+	if ((char *)pSrc < (char *)pDst && (char *)pSrc + nLength >= (char *)pDst)
+	{
+		int i = nLength;
+		while (i-- > 0)
+		{
+			*((char *)pDst + i) = *((char *)pSrc + i);
+		}
+	}
+	else
+	{
+		int i = 0;
+		while (i < nLength)
+		{
+			*((char *)pDst + i) = *((char *)pSrc + i);
+			i++;
+		}
+	}
+	return pDst;
+}
+
+int main()
+{
+	const int LEN = 10;
+	char test[LEN];
+	for (int i = 0; i < LEN; i++)
+	{
+		test[i] = (i % 10) + '0';
+	}
+	for (int i = 0; i < LEN; i++) cout << test[i];
+	cout << endl;
+
+	//memcpy(&test[1], &test[3], 5);
+	memcpy(&test[3], &test[1], 9);
+
+	
+	for (int i = 0; i < LEN; i++) cout << test[i];
+	cout << endl;
+}

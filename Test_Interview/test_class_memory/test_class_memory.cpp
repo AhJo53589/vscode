b240a08e76ca62a735f72787cb3cfc86e81cc666
AhJo53589 <ahjo_bb@hotmail.com>
2019-09-03 22:14:01 +0800

    update

--- Test_Interview/test_class_memory/test_class_memory.cpp
+++ Test_Interview/test_class_memory/test_class_memory.cpp
@@ -4,28 +4,6 @@
 #include "pch.h"
 // #include <iostream>
 
-//class Base
-//{
-//	int a;
-//	int b;
-//public:
-//	void CommonFunction();
-//	void virtual VirtualFunction();
-//};
-//
-//class DerivedClass : public Base
-//{
-//	int c;
-//public:
-//	void DerivedCommonFunction();
-//	//void virtual VirtualFunction();
-//	void virtual VirtualFunction2();
-//};
-
-
-
-//////////////////////////////////////////////////////////////////////////
-// 多重继承
 class Base
 {
 	int a;
@@ -35,30 +13,64 @@ public:
 	void virtual VirtualFunction();
 };
 
-
-class DerivedClass1 : public Base
+class DerivedClass : public Base
 {
 	int c;
 public:
 	void DerivedCommonFunction();
-	void virtual VirtualFunction();
+	//void virtual VirtualFunction();
+	void virtual VirtualFunction2();
 };
 
-class DerivedClass2 : public Base
-{
-	int d;
-public:
-	void DerivedCommonFunction();
-	void virtual VirtualFunction();
-};
 
-class DerivedDerivedClass : public DerivedClass1, public DerivedClass2
-{
-	int e;
-public:
-	void DerivedDerivedCommonFunction();
-	void virtual VirtualFunction();
-};
+
+//////////////////////////////////////////////////////////////////////////
+// 多重继承
+//class Base
+//{
+//	int a;
+//	int b;
+//public:
+//	void CommonFunction();
+//	void virtual VirtualFunction();
+//};
+//
+//
+//class DerivedClass1 : public Base
+//{
+//	int c;
+//public:
+//	void DerivedCommonFunction();
+//	void virtual VirtualFunction();
+//};
+//
+//class DerivedClass2 : public Base
+//{
+//	int d;
+//public:
+//	void DerivedCommonFunction();
+//	void virtual VirtualFunction();
+//};
+//
+//class DerivedDerivedClass : public DerivedClass1, public DerivedClass2
+//{
+//	int e;
+//public:
+//	void DerivedDerivedCommonFunction();
+//	void virtual VirtualFunction();
+//};
+
+//////////////////////////////////////////////////////////////////////////
+// 结构体
+//struct Base
+//{
+//	void virtual VirtualFunction();
+//};
+//
+//struct DerivedClass : public Base
+//{
+//	void virtual VirtualFunction();
+//};
 
 int main()
 {

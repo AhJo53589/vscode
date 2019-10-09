958386451a10961abc5b39da2550d9d09b0ed414
Ikaruga <ahjo_bb@hotmail.com>
2019-10-09 16:40:03 +0800

    update

---
+++ test_cpp_primer/Chapter_10_Generic_Algorithms/Chapter_10_Generic_Algorithms.cpp
@@ -0,0 +1,192 @@
+﻿// Chapter_10_Generic_Algorithms.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iterator>
+
+#include <vector>
+#include <string>
+#include <list>
+
+using namespace std;
+
+void elimDups(vector<string> &words)
+{
+	sort(words.begin(), words.end());
+	auto end_unique = unique(words.begin(), words.end());
+	words.erase(end_unique, words.end());
+}
+
+void biggies(vector<string> words, vector<string>::size_type sz)
+{
+	cout << "words = " << endl;
+	for (auto i : words) cout << i << ","; cout << endl;
+
+	elimDups(words);
+	cout << "words (sort by dict) = " << endl;
+	for (auto i : words) cout << i << ","; cout << endl;
+
+	stable_sort(words.begin(), words.end(), [](const string &s1, const string &s2)
+	{
+		return s1.size() < s2.size();
+	});
+	cout << "words (stable_sort by size) = " << endl;
+	for (auto i : words) cout << i << ","; cout << endl;
+
+	auto wc = find_if(words.begin(), words.end(), [sz](const string &a)
+	{
+		return a.size() >= sz;
+	});
+	auto count = words.end() - wc;
+	auto make_plural = [](size_t ctr, const string &word, const string &ending)
+	{
+		return (ctr <= 1) ? word : word + ending;
+	};
+	cout << count << " " << make_plural(count, "word", "s") << " of length " << sz << " or longer" << endl;
+
+	for_each(wc, words.end(), [](const string &s)
+	{
+		cout << s << ",";
+	});
+	cout << endl;
+
+	cout << "words partition (size > 5) = " << endl;
+	auto it = partition(words.begin(), words.end(), [](const string &s)
+	{
+		return s.size() > 5;
+	});
+	for (auto i : words) cout << i << ","; cout << endl;
+	cout << "[" << *it << "]" << endl;
+}
+
+int main()
+{
+	{
+		vector<int> vec = { 1,2,3,4 };
+		int sum = accumulate(vec.cbegin(), vec.cend(), 0);	// #include <numeric>
+		cout << "accumulate = " << sum << endl;
+	}
+
+	{
+		vector<string> v = { "Hello", "AhJo" };
+		string sum = accumulate(v.cbegin(), v.cend(), string("+"));
+		cout << "accumulate = " << sum << endl;
+	}
+
+	{
+		vector<int> a = { 1,2,3,4,5 };
+		vector<int> b = { 1,2,3,4,5,6,7,8,9 };
+		cout << "equal (a,b) = " << boolalpha << equal(a.cbegin(), a.cend(), b.cbegin()) << endl;
+	}
+
+	{
+		vector<int> vec = { 1,2,3,4 };
+		fill(vec.begin(), vec.end(), 0);
+		for (auto i : vec) cout << i << ",";
+		cout << endl;
+		fill_n(vec.begin(), 2, 1);
+		for (auto i : vec) cout << i << ","; cout << endl;
+	}
+
+	{
+		vector<int> vec;
+		auto it = back_inserter(vec);
+		*it = 1;
+		fill_n(back_inserter(vec), 10, 2);
+		for (auto i : vec) cout << i << ","; cout << endl;
+	}
+
+	{
+		int a1[] = { 0,1,2,3,4,5,6,7,8,9 };
+		int a2[sizeof(a1) / sizeof(*a1)];
+		// ret指向拷贝到a2的尾元素之后的位置
+		auto ret = copy(begin(a1), end(a1), a2);
+	}
+
+	{
+		vector<int> vec = { 1,2,3,4,5,1,2,3,4,5 };
+		replace(vec.begin(), vec.end(), 1, 2);
+		list<int> lst;
+		replace_copy(vec.cbegin(), vec.cend(), back_inserter(lst), 2, 3);
+		for (auto i : lst) cout << i << ","; cout << endl;
+	}
+
+	{
+		vector<string> words = { "the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle" };
+		biggies(words, 4);
+	}
+
+	{
+		vector<string> words = { "a", "a", "b", "c", "d", "e", "e", "e", "f", "g" };
+		vector<string> n;
+		unique_copy(words.cbegin(), words.cend(), back_inserter(n));
+		for (auto i : n) cout << i << ","; cout << endl;
+	}
+
+	{
+		string str = "MyLove";
+		vector<int> vec = { 5,5,6,8,5,4,9,5,2,4 };
+		auto length = str.size();
+		auto f = [](const int &a, const size_t t)
+		{
+			return a > t;
+		};
+		auto it = find_if(vec.begin(), vec.end(), bind(f, placeholders::_1, length)); // #include <functional>
+		cout << "fist val > MyLove length = " << *it << endl;
+	}
+
+	{
+		list<int> lst = { 1,2,3,4 };
+		list<int> lst2;
+		list<int> lst3;
+		copy(lst.cbegin(), lst.cend(), front_inserter(lst2));	// #include <iterator>
+		copy(lst.cbegin(), lst.cend(), inserter(lst3, lst3.begin()));
+		for (auto l : lst) cout << l << ","; cout << endl;
+		for (auto l : lst2) cout << l << ","; cout << endl;
+		for (auto l : lst3) cout << l << ","; cout << endl;
+	}
+
+	//{
+	//	istream_iterator<int> in(cin), eof;
+	//	cout << accumulate(in, eof, 0) << endl;
+	//}
+
+	{
+		string line = { "FIRST,MIDDLE,LAST" };
+		auto rcomma = find(line.crbegin(), line.crend(), ',');
+		cout << string(rcomma.base(), line.cend()) << endl;
+	}
+
+	{
+		list<int> lst = { 1,2,3,4,5,6,7,8,9 };
+		list<int> lst2 = { 11,12,13,14,15 };
+		auto it_1 = lst.begin();
+		auto it_2 = lst2.begin();
+		it_1++;
+		it_2++;
+		//lst.merge(lst2);
+		lst.remove(1);
+		//lst.sort();
+		//lst.unique();
+		//lst.splice(it_1, lst2);
+		//lst.splice(it_1, lst2, it_2);
+		lst.splice(it_1, lst2, it_2, lst2.end());
+		for (auto l : lst) cout << l << ","; cout << endl;
+		for (auto l : lst2) cout << l << ","; cout << endl;
+	}
+
+	{
+		vector<int> vec(10, 0);
+		iota(vec.begin(), vec.end(), 1);
+		for (auto i : vec) cout << i << ","; cout << endl;
+		vector<int> psum, adj;
+		partial_sum(vec.cbegin(), vec.cend(), back_inserter(psum));
+		for (auto i : psum) cout << i << ","; cout << endl;
+		adjacent_difference(psum.cbegin(), psum.cend(), back_inserter(adj));
+		for (auto i : adj) cout << i << ","; cout << endl;
+	}
+}
---
+++ test_cpp_primer/Chapter_11_Associative_Containers/Chapter_11_Associative_Containers.cpp
@@ -0,0 +1,164 @@
+﻿// Chapter_11_Associative_Containers.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const string & transform(const string &s, const map<string, string> &m)
+{
+	auto map_it = m.find(s);
+	if (map_it != m.cend())
+	{
+		return map_it->second;
+	}
+	else
+	{
+		return s;
+	}
+}
+
+map<string, string> buildMap(ifstream &map_file)
+{
+	map<string, string> trans_map;
+	string key;
+	string value;
+	while (map_file >> key && getline(map_file, value))
+	{
+		if (value.size() > 1)
+		{
+			trans_map[key] = value.substr(1);
+		}
+		else
+		{
+			throw runtime_error("no rule for " + key);
+		}
+	}
+	return trans_map;
+}
+
+void word_transform(ifstream &map_file, ifstream &input)
+{
+	auto trans_map = buildMap(map_file);
+	string text;
+	while (getline(input, text))
+	{
+		istringstream stream(text);
+		string word;
+		bool firstword = true;
+		while (stream >> word)
+		{
+			if (firstword)
+			{
+				firstword = false;
+			}
+			else
+			{
+				cout << " ";
+			}
+			cout << transform(word, trans_map);
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	{
+		set<string>::value_type v1;
+		set<string>::key_type v2;
+		map<string, int>::value_type v3;
+		map<string, int>::key_type v4;
+		map<string, int>::mapped_type v5;
+	}
+
+	{
+		map<int, int> m;
+		auto b = m.insert({ 1,1 });
+		cout << "return from map insert. [using _Pairib = pair<iterator, bool>;]" << endl;
+		cout << "first = (" << (b.first)->first << "," << (b.first)->second << ")" << endl;
+		cout << "second = " << boolalpha << b.second << endl;
+	}
+
+	{
+		multimap<int, int> m = { {1,1},{1,2},{1,3},{2,2} };
+		auto cnt = m.erase(1);
+		cout << "erase count = " << cnt << endl;
+	}
+
+	{
+		set<int> s = { 1,2,3,4,5,6,7,8,9 };
+		auto it = s.find(5);
+		cout << "set find 5 = " << *it << endl;
+		it = s.lower_bound(5);
+		cout << "set lower_bound 5 = " << *it << endl;
+		it = s.upper_bound(5);
+		cout << "set upper_bound 5 = " << *it << endl;
+		auto it_pair = s.equal_range(5);
+		cout << "set equal_range 5, first = " << *it_pair.first << ", second = " << *it_pair.second << endl;
+	}
+
+	{
+		// 返回满足条件的合集
+		multimap<int, int> m = { {1,1},{1,2},{2,1},{2,2},{2,3},{3,1} };
+		cout << "print multimap elements by count and find: " << endl;
+		auto entries = m.count(2);
+		auto it = m.find(2);
+		while (entries)
+		{
+			cout << it->second << ",";
+			++it;
+			--entries;
+		}
+		cout << endl;
+
+		cout << "print multimap elements by lower_bound & upper_bound: " << endl;
+		for (auto beg = m.lower_bound(2), end = m.upper_bound(2); beg != end; ++beg)
+		{
+			cout << beg->second << ",";
+		}
+		cout << endl;
+
+		cout << "print multimap elements by equal_range: " << endl;
+		for (auto pos = m.equal_range(2); pos.first != pos.second; ++pos.first)
+		{
+			cout << pos.first->second << ",";
+		}
+		cout << endl;
+	}
+
+	{
+		// 单词转换程序
+		ifstream map_file("trans.txt");
+		ifstream input("text.txt");
+		word_transform(map_file, input);
+	}
+
+	{
+		unordered_map<string, size_t> word_count;
+		vector<string> word = { "d", "e", "d", "a", "b", "c", "b", "c" };
+		for (const auto &w : word)
+		{
+			word_count[w]++;
+		}
+		for (const auto &w : word_count)
+		{
+			cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times" : " time") << endl;
+		}
+
+		cout << "bucket_count = " << word_count.bucket_count() << endl;
+		cout << "max_bucket_count = " << word_count.max_bucket_count() << endl;
+		cout << "bucket_size(0) = " << word_count.bucket_size(0) << endl;
+		cout << "bucket(a) = " << word_count.bucket("a") << endl;
+	}
+}
